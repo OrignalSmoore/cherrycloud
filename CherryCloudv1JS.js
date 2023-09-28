19d0b0974e93f37870512a33f026116d221103fe
OrignalSmoore <spfm993@gmail.com>
2023-09-28 09:56:28 -0400

    Add files via upload

--- CherryCloudv1JS.js
+++ CherryCloudv1JS.js
@@ -341,79 +341,236 @@ function collectLeasingData() {
 	console.log(debtService);
 
 	/*const effectiveGrossIncome(Gross Potential Rent - Vacancy = Total Rent Revenue + Operating Income);*/
-	
+	let grossPotentialRent = grossPotentialRent;
+	let vacancy = vacancy;
+	let totalRentRevenue = totalRentRevenue;
+	let operatingIncome = operatingIncome;
+	let effectiveGrossIncome = (grossPotentialRent  - vacancy) = totalRentRevenue + operatingIncome;
+	console.log(effectiveGrossIncome);
 
 	/*const effectiveMarketRent ((Monthly Market Rent x # of months in lease - total concessions)/ # of months in lease);*/
+	let monthlyMarketRent = monthlyMarketRent;
+	let numberOfMonthsInLease = numberOfMonthsInLease;
+	let totalConcessions = totalConcessions;
+	let effectiveMarketRent = ((monthlyMarketRent * numberOfMonthsInLease) - totalConcessions) / numberOfMonthsInLease;
+	console.log(effectiveMarketRent);
 
 	/*const grossPotentialIncome(Occupied Units x Average Leased Rent + Vacant Units x Average Market Rent);*/
+	let occupiedUnits = occupiedUnits;
+	let averageLeasedRent = averageLeasedRent;
+	let vacantUnits = vacantUnits;
+	let averageMarketRent = averageMarketRent;
+	let grossPotentialIncome = occupiedUnits * averageLeasedRent * vacantUnits * averageMarketRent;
+	console.log(grossPotentialRent);
 
 	/*const grossPotentialRent (Total Units x Average Market Rent);*/
+	let totalUnits = totalUnits;
+	let averageMarketRent = averageMarketRent;
+	let grossPotentialRent = totalUnits * averageMarketRent;
+	console.log(grossPotentialRent);
 
 	/*const leasedPercentage (Leased Units / Total Units);*/
+	let leasedUnits = leasedUnits;
+	let totalUnits = totalUnits;
+	let leasedPercentage = leasedUnits / totalUnits;
+	console.log(leasedPercentage);
 
 	/*const leasedUnits (Total Units - Vacant Units - Notices to Vacate + Vacant Units Leased + Notices to Vacate Preleased);*/
+	let totalUnits = totalUnits;
+	let vacantUnitsLeased = vacantUnitsLeased;
+	let noticesToVacate = nTV;
+	let noticesToVacatePreLeased = preleasedNTV;
+	let leasedUnits = (totalUnits - vacantUnits - nTV) + vacantUnitsLeased + preleasedNTV;
+	console.log(leasedUnits);
 
 	/*const netOperatingIncome (Effective Gross Income - Operating Expenses);*/
+	let effectiveGrossIncome = effectiveGrossIncome;
+	let operatingExpense = operatingExpense;
+	let netOperatingIncome = effectiveGrossIncome - operatingExpense;
+	console.log(netOperatingIncome);
 
 	/*const operatingExpenseRatio (Operating Expense / Effective Gross Income);*/
+	let operatingExpense = operatingExpense;
+	let effectiveGrossIncome = effectiveGrossIncome;
+	let operatingExpenseRatio = operatingExpense / effectiveGrossIncome;
+	console.log(operatingExpenseRatio)
 
 	/*const percentRenewalIncrease (Amount of Increase / Previous Lease Rent);*/
+	let amountOfIncrease = amountOfIncrease;
+	let previousLeaseRent = previousLeaseRent;
+	let percentRenewalIncrease = amountOfIncrease / previousLeaseRent;
+	console.log(percentRenewalIncrease);
 
 	/*const replacementReserve (amount set asided for future expenses and projects);*/
+	const replacementReserve = prompt("What is the amount set aside for furture expenses and projects?");
+	console.log(replacementReserve);
 
 	/*const returnOnInvestment (Return / Investment);*/
+	let returnIncome = returnIncome;
+	let investment = investment;
+	let returnOnInvestment = returnIncome / investment;
+	console.log(returnOnInvestment);
 
 	/*const totalRentRevenue (Gross Potential Rent - Vacancy);*/
+	let grossPotentialRent = grossPotentialRent;
+	let vacancy = vacancy;
+	let totalRentRevenue = grossPotentialRent - vacancy;
+	console.log(totalRentRevenue);
 
 	/*const turnoverRatio (Total Number of Move Outs / Total Number of Units);*/
+	let totalNumberOfMoveOuts = totalNumberOfMoveOuts;
+	let totalNumberOfUnits = totalNumberOfUnits;
+	let turnoverRatio = totalNumberOfMoveOuts / totalNumberOfUnits;
+	console.log(turnoverRatio);
 
 	/*const vacancy/collectionLoss (Vacancy, Collection Loss, Non Revenue Units, and Concessions);*/
+	const collectionLoss = prompt("What is the total collection loss? This would be the total vacancy cost, collection loss, non revenue units and concessions.")
+	console.log(collectionLoss);
 
 /*Aquisitions*/
     /*const interestForLoan (Interest= Principal x Rate x Time);*/
+	let principal = principal;
+	let rate = rate;
+	let time = time;
+	let interestForLoan = principal * rate * time;
+	console.log(interestForLoan);
 
 	/*const principalOnLoan ([Principal = Interest ÷ (% Rate x Time)]);*/
+	let interest = interest;
+	let percentRate = percentRate;
+	let time = time;
+	let principalOnLoan = interest / (percentRate * time);
+	console.log(principalOnLoan)
 
 	/*const rateOnLoan ([Rate = Interest ÷ (Principal x Time)]);*/
+	let interest = interest;
+	let principalOnLoan = principalOnLoan;
+	let time = time;
+	let rateOnLoan = interest / (principalOnLoan * time);
+	console.log(rateOnLoan);
 
 	/*const netOperatingIncome (NOI = Property Value x Cap Rate);*/
+	let propertyValue = propertyValue;
+	let capRate = capRate;
+	let netOperatingIncome = propertyValue * capRate;
+	console.log(netOperatingIncome);
 
 	/*const propertyValue (Property Value = NOI ÷ Cap Rate);*/
+	let netOperatingIncome = netOperatingIncome;
+	let capRate = capRate;
+	let propertyValue = netOperatingIncome / capRate;
+	console.log(propertyValue);
 
 	/*const capRate (Cap Rate = NOI ÷ Property Value);*/
+	let netOperatingIncome = propertyValue;
+	let propertyValue = propertyValue;
+	let capRate = netOperatingIncome / propertyValue;
+	console.log(capRate);
 
 	/*const income(Income = Amount Paid x Rate of Return);*/
+	let amountPaid = amountPaid;
+	let rateOfReturn = rateOfReturn;
+	let income = amountPaid * rateOfReturn;
+	console.log(income);
 
 	/*const amountPaid (Amount Paid = Income ÷ Rate of Return);*/
+	let income = income;
+	let rateOfReturn = rateOfReturn;
+	let amountPaid = income / rateOfReturn;
+	console.log(amountPaid);
 
 	/*const rateOfReturn (Rate of Return = Income ÷ Amount Paid);*/
+	let income = income;
+	let amountPaid = amountPaid;
+	let rateOfReturn = income / amountPaid;
+	console.log(rateOfReturn);
 
 	/*const increaseInValue (Increase = Purchase Price x Rate of Return);*/
+	let purchasePrice = purchasePrice;
+	let rateOfReturn = rateOfReturn;
+	let increaseInValue = purchasePrice * rateOfReturn;
+	console.log(increaseInValue);
 
 	/*const purchasePrice (Purchase Price = Increase ÷ Rate of Return);*/
+	let increaseInValue = increaseInValue;
+	let rateOfReturn = rateOfReturn;
+	let purchasePrice = increaseInValue / rateOfReturn;
+	console.log(purchasePrice);
 
 	/*const rateOfReturn (Rate = Increase ÷ Purchase Price);*/
+	let increaseInValue = increaseInValue;
+	let purchasePrice = purchasePrice;
+	let rateOfReturn = increaseInValue / purchasePrice;
+	console.log(rateOfReturn);
 
 	/*const loanToValue (LTV= Loan Amount / Sales price or appraised value (whichever is less));*/
+	let loanAmount = loanAmount;
+	let salesPriceOrAppraisedPrice = salesPriceOrAppraisedPrice;
+	let loanToValue = loanAmount / salesPriceOrAppraisedPrice;
+	console.log(loanToValue);
 
 	/*const loanAmount (Sales Price or appraised value(whichever is less)x Loan-to-Value(LTV));*/
+	let salesPriceOrAppraisedPrice = salesPriceOrAppraisedPrice;
+	let loanToValue = loanToValue;
+	let loanAmount = salesPriceOrAppraisedPrice * loanToValue;
+	console.log(loanAmount);
 
 	/*const salesPriceOrAppraisedPrice(Loan amount / Loan-to-Value(LTV));*/
+	let loanAmount = loanAmount;
+	let loanToValue = loanToValue;
+	let salesPriceOrAppraisedPrice = loanAmount / loanToValue;
+	console.log(salesPriceOrAppraisedPrice);
 
 	/*const annualInterest(loan amount(Principal) x annual interest rate);*/
+	let loanAmount = loanAmount;
+	let annualInterestRate = annualInterestRate;
+	let annualInterest = loanAmount * annualInterestRate;
+	console.log(annualInterest);
 
 	/*const principalLoanAmount (Annual Interest / Annual Interest Rate);*/
+	let annualInterest = annualInterest;
+	let annualInterestRate = annualInterestRate;
+	let principalLoanAmount = annualInterest / annualInterestRate;
+	console.log(principalLoanAmount);
 
 	/*const annualInterestRate (Annual Interest / Loan Amount(principal));*/
+	let annualInterest = annualInterest;
+	let loanAmount = loanAmount;
+	let annualInterestRate = annualInterestRate;
+	console.log(annualInterestRate);
 
 	/*const amountPaidForPoints (Loan amount x Points as Percentage of Loan);*/
+	let loanAmount = loanAmount;
+	let pointsAsPercentageOfLoan = pointsAsPercentageOfLoan;
+	let amountPaidForPoints = loanAmount * pointsAsPercentageOfLoan;
+	console.log(amountPaidForPoints);
 
 	/*const loanAmount(Amount Paid for Points / Points as Percentage of Loan);*/
+	let amountPaidForPoints = amountPaidForPoints;
+	let pointsAsPercentageOfLoan = pointsAsPercentageOfLoan;
+	let loanAmount = amountPaidForPoints / pointsAsPercentageOfLoan;
+	console.log(loanAmount);
 
 	/*const pointsAsPercentageOfLoan (Amount Paid for Points / Loan Amount);*/
+	let amountPaidForPoints = amountPaidForPoints;
+	let loanAmount = loanAmount;
+	let pointsAsPercentageOfLoan = amountPaidForPoints / loanAmount;
+	console.log(pointsAsPercentageOfLoan);
 
 	/*const percentageRent (Gross sales subject to percentage x % in the lease);*/
+	let grossSalesSubjectToThePercentage = grossSalesSubjectToThePercentage;
+	let percentInTheLease = percentInTheLease;
+	let percentageRent = grossSalesSubjectToThePercentage * percentInTheLease;
+	console.log(percentageRent);
 
 	/*const grossSalesSubjectToThePercentage (Percentage Rent / % in the lease);*/
+	let percentageRent = percentageRent;
+	let percentInTheLease = percentInTheLease;
+	let grossSalesSubjectToThePercentage = percentageRent / percentInTheLease;
+	console.log(grossSalesSubjectToThePercentage);
 
 	/*const percentInTheLease (Percentage Rent / Gross Sales Suject to the Percentage);*/
+	let percentageRent = percentageRent;
+	let grossSalesSubjectToThePercentage = grossSalesSubjectToThePercentage;
+	let percentInTheLease = percentageRent / grossSalesSubjectToThePercentage;
+	console.log(percentInTheLease);
